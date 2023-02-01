9cbaa573a81c84203940da5b4a724fb9c3119273
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2023-01-31 20:28:07 -0600

    reto logrado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,89 +1,26 @@
 import { Component } from '@angular/core';
-import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  btnDisabled: boolean = true;
-  progressBar: number = 50;
-  btnText = 'Enable';
+  todo: string = '';
 
-  person = {
-    name: 'Mike',
-    age: 24,
-    urlImage: 'https://source.unsplash.com/random',
+  todos: string[] = [];
+
+  doneTodos: string[] = [];
+
+  finishedTodo: string[] = [];
+
+  createTodo() {
+    this.todos.push(this.todo);
+    this.todo = '';
   }
 
-  names: string[] = ['Pepe', 'Antonio', 'Juan'];
-  newName: string = '';
-
-  products: Product[] = [
-    {
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg',
-      category: 'toys',
-    },
-    {
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg'
-    },
-    {
-      name: 'Casa para perro',
-      price: 34,
-      image: './assets/images/house.jpg'
-    },
-    {
-      name: 'Gafas',
-      price: 3434,
-      image: './assets/images/glasses.jpg'
-    }
-  ]
-
-  toggleButton() {
-    this.btnDisabled === true ? this.btnText = 'Disable' : this.btnText = 'Enable';
-
-    this.btnDisabled = !this.btnDisabled;
-  }
-
-  incrementAge() {
-    this.person.age += 1;
-  }
-
-  decrementAge() {
-    this.person.age -= 1;
-  }
-
-  onScroll(event: Event) {
-    const element = event.target as HTMLElement;
-    console.log(element.scrollTop);
-  }
-
-  changeName(event: Event) {
-    const element = event.target as HTMLInputElement;
-    this.person.name = element.value;
-  }
-
-  addName() {
-    this.names.push(this.newName);
-    this.newName = '';
-  }
-
-  removeName(index: number) {
-    this.names.splice(index, 1);
+  completeTodo(index: number) {
+    this.finishedTodo = this.todos.splice(index, 1);
+    this.doneTodos.push(this.finishedTodo[0]);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'; // requerido para ngModule
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,7 +11,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule // requerido para ngModule
+    FormsModule, // requerido para ngModule
   ],
   providers: [],
   bootstrap: [AppComponent]
